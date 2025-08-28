1254afdd4a74b7ed58a33ce07a4370899ec67f96
itsChisomm <chisomc@hotmail.co.uk>
2025-08-28 13:00:45 +0100

    Rotation & Thrust Input

--- Rocket Boost/Assets/Scripts/PlayerController.cs
+++ Rocket Boost/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,64 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-    bool isThrusting = false;
+    [SerializeField] InputAction rotation;
+
+
+    [SerializeField] private float thrustForce = 10f;
+    [SerializeField] private float rotationForce = 100f;
+
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void OnEnable()
     {
         thrust.Enable();
-    }   
-   
-    private void OnDisable()
-    {
-        thrust.Disable();
+        rotation.Enable();
     }
 
-    void Update()
+    /* private void OnDisable()
+     {
+         thrust.Disable();
+         rotation.Disable();
+     } */
+
+    private void FixedUpdate()
     {
-       if (thrust.IsPressed())
-       {
-           isThrusting = true;
-           Debug.Log("Thrusting");
-       }
-       else
-       {
-           isThrusting = false;
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationForce = rotation.ReadValue<float>();
+
+        if (rotationForce < 0f)
+        {
+            ApplyRotation(rotationForce);
+        }
+
+        else if (rotationForce > 0f)
+
+        {
+            ApplyRotation(-rotationForce);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.AddRelativeTorque(Vector3.forward * rotationThisFrame  * Time.fixedDeltaTime);
     }
 }
